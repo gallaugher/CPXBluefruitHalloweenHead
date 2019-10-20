8c82b355232936f703327b1c71dbd5f9e0dda57d
Gallaugher <john.gallaugher@bc.edu>
2019-10-20 12:46:33 -0400

    Update code.py
    cap touch on A1 will scream "Don't take candy from strangers!". Bluefruit App control pad #1 - #3 light up CPXb and play a sound summoning the potential candy-taker.
    Must include files:
    "hey.wav"
    "hey_you.wav"
    "take_candy.wav"
       and
    "ah_dont_take_candy_from_strangers.wav"

--- code.py
+++ code.py
@@ -1,66 +1,83 @@
 import board
 import neopixel
-import time
 import digitalio
-import adafruit_lis3dh
+import analogio
+import simpleio
 import busio
 import touchio
+import time
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_ble.uart_server import UARTServer
+from audiopwmio import PWMAudioOut as AudioOut
+from audiocore import WaveFile
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.5, auto_write=True)
+from adafruit_bluefruit_connect.packet import Packet
+from adafruit_bluefruit_connect.color_packet import ColorPacket
+from adafruit_bluefruit_connect.button_packet import ButtonPacket
 
+# set up touchpads A1
+touchpad_A1 = touchio.TouchIn(board.A1)
+
+# setup pixels
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.3, auto_write=True)
+
+# name colors so you don't need to refer to numbers
 RED = (255, 0, 0)
 ORANGE = (255, 50, 0)
-YELLOW = (255, 165, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-INDIGO = (50, 0, 255)
-VIOLET = (128, 0, 128)
 BLACK = (0, 0, 0)
 
-colors = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET, BLACK]
+# setup bluetooth
+uart_server = UARTServer()
 
-touch_A1 = touchio.TouchIn(board.A1)
-
-try:
-    from audiocore import WaveFile
-except ImportError:
-    from audioio import WaveFile
-
-try:
-    from audioio import AudioOut
-except ImportError:
-    try:
-        from audiopwmio import PWMAudioOut as AudioOut
-    except ImportError:
-        pass  # not always supported by every board!
-
-# Enable the speaker
-spkrenable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
-spkrenable.direction = digitalio.Direction.OUTPUT
-spkrenable.value = True
-
-button_A = digitalio.DigitalInOut(board.BUTTON_A)
-button_A.switch_to_input(pull=digitalio.Pull.DOWN)
-
-def play_file(filename):
+def playfile(filename):
     wave_file = open(filename, "rb")
     with WaveFile(wave_file) as wave:
         with AudioOut(board.SPEAKER) as audio:
             audio.play(wave)
             while audio.playing:
-                 pass
-    print("Finished")
+                pass
+
+def checkTouch():
+    # check if touched. If so, play sound
+    if touchpad_A1.value:
+        print("Touched!")
+        pixels.fill(ORANGE)
+        playfile("ah_dont_take_candy_from_strangers.wav")
+        pixels.fill(BLACK)
 
 while True:
-    if touch_A1.value:
-        pixels.fill(ORANGE)
-        play_file("evil_laugh.wav")
-        pixels.fill(BLACK)
-        time.sleep(0.5)
+    # set CPXb up so that it can be discovered by the app
+    uart_server.start_advertising()
+    while not uart_server.connected:
+        checkTouch()
+        pass
 
-    if button_A.value:
-        pixels.fill(RED)
-        play_file("laugh.wav")
-        pixels.fill(BLACK)
-        time.sleep(0.5)
+    # Now we're connected
+
+    while uart_server.connected:
+
+        # check if touched. If so, play sound
+        checkTouch()
+            
+        if uart_server.in_waiting:
+            try:
+                packet = Packet.from_stream(uart_server)
+            except ValueError:
+                continue # or pass. This will start the next iteration of the loop and try to get a valid packet again.
+            
+            if isinstance(packet, ColorPacket): # check if a color was sent from color picker
+                pixels.fill(packet.color)
+            if isinstance(packet, ButtonPacket): # check if a button was pressed from control pad
+                if packet.pressed:
+                    if packet.button == ButtonPacket.BUTTON_1: # if button #1
+                        pixels.fill(RED)
+                        playfile("hey.wav")
+                        pixels.fill(BLACK)
+                    if packet.button == ButtonPacket.BUTTON_2: # if button #2
+                        pixels.fill(ORANGE)
+                        playfile("hey_you.wav")
+                        pixels.fill(BLACK)
+                    if packet.button == ButtonPacket.BUTTON_3: # if button #2
+                        pixels.fill(RED)
+                        playfile("take_candy.wav")
+                        pixels.fill(BLACK)
