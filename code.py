9af36fb580c1b3c824bad76eff9b2ab3864a0689
Gallaugher <john.gallaugher@bc.edu>
2021-10-07 15:47:46 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,20 @@
 # By Prof. John Gallaugher as a demo of fun things you can do with the 
-# Adafruit Circuit Playground Express Bluefruit
-# for more projects, see: https://bit.ly/GallaugherYouTube
+# Adafruit Circuit Playground Bluefruit
+# for more projects, see: https://YouTube.com/profgallaugher
 # Twitter: @gallaugher, or gallaugher.com
-# video of build at: http://bit.ly/cpxb-halloween-head-video
+# an old video of build at: http://bit.ly/cpxb-halloween-head-video
+# but the code below has been updated for CircuitPython 7.0
+# I'll try to get a new build video up at some point.
+#
+# Sound files are on YouTube. Put them in your CPB in a folder
+# named sounds. Sound files are: 
+# hey.wav
+# hey_you.wav
+# take_candy.wav
+# theres_candy_in_my_head.wav
+# open_my_head.wav
+# ah_dont_take_candy_from_strangers.wav
+
 import board
 import neopixel
 import digitalio
@@ -12,13 +24,15 @@ import busio
 import touchio
 import time
 from digitalio import DigitalInOut, Direction, Pull
-from adafruit_ble.uart_server import UARTServer
-from audiopwmio import PWMAudioOut as AudioOut
-from audiocore import WaveFile
-
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
 from adafruit_bluefruit_connect.packet import Packet
 from adafruit_bluefruit_connect.color_packet import ColorPacket
 from adafruit_bluefruit_connect.button_packet import ButtonPacket
+# import lines needed to play sound files
+from audiopwmio import PWMAudioOut as AudioOut
+from audiocore import WaveFile
 
 # set up touchpads A1
 touchpad_A1 = touchio.TouchIn(board.A1)
@@ -32,35 +46,47 @@ ORANGE = (255, 50, 0)
 BLACK = (0, 0, 0)
 
 # setup bluetooth
-uart_server = UARTServer()
+ble = BLERadio()
+uart_server = UARTService()
+advertisement = ProvideServicesAdvertisement(uart_server)
+# Give your CPB a unique name between the quotes below
+advertisement.complete_name = "skull"
 
-def playfile(filename):
-    wave_file = open(filename, "rb")
-    with WaveFile(wave_file) as wave:
-        with AudioOut(board.SPEAKER) as audio:
-            audio.play(wave)
-            while audio.playing:
-                pass
+# set up the speaker
+speaker = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
+speaker.direction = digitalio.Direction.OUTPUT
+speaker.value = True
+audio = AudioOut(board.SPEAKER)
+
+# set path where sound files can be found
+path = "sounds/"
+
+def play_sound(filename):
+    with open(path + filename, "rb") as wave_file:
+        wave = WaveFile(wave_file)
+        audio.play(wave)
+        while audio.playing:
+            pass
 
 def checkTouch():
     # check if touched. If so, play sound
     if touchpad_A1.value:
         print("Touched!")
         pixels.fill(ORANGE)
-        playfile("ah_dont_take_candy_from_strangers.wav")
+        play_sound("ah_dont_take_candy_from_strangers.wav")
         pixels.fill(BLACK)
 
 while True:
-    # set CPXb up so that it can be discovered by the app
-    uart_server.start_advertising()
-    while not uart_server.connected:
+    ble.start_advertising(advertisement)
+    while not ble.connected:
         checkTouch()
-        pass
 
+    # Now we're connected so we don't need to advertise the device as one that you're able to connect to
+    ble.stop_advertising()
+    
     # Now we're connected
 
-    while uart_server.connected:
-
+    while ble.connected:
         # check if touched. If so, play sound
         checkTouch()
             
@@ -68,29 +94,29 @@ while True:
             try:
                 packet = Packet.from_stream(uart_server)
             except ValueError:
-                continue # or pass. This will start the next iteration of the loop and try to get a valid packet again.
-            
+                continue # or pass. This will start the next
+
             if isinstance(packet, ColorPacket): # check if a color was sent from color picker
                 pixels.fill(packet.color)
             if isinstance(packet, ButtonPacket): # check if a button was pressed from control pad
                 if packet.pressed:
                     if packet.button == ButtonPacket.BUTTON_1: # if button #1
                         pixels.fill(RED)
-                        playfile("hey.wav")
+                        play_sound("hey.wav")
                         pixels.fill(BLACK)
                     if packet.button == ButtonPacket.BUTTON_2: # if button #2
                         pixels.fill(ORANGE)
-                        playfile("hey_you.wav")
+                        play_sound("hey_you.wav")
                         pixels.fill(BLACK)
                     if packet.button == ButtonPacket.BUTTON_3: # if button #2
                         pixels.fill(RED)
-                        playfile("take_candy.wav")
+                        play_sound("take_candy.wav")
                         pixels.fill(BLACK)
                     if packet.button == ButtonPacket.BUTTON_4: # if button #2
                         pixels.fill(ORANGE)
-                        playfile("theres_candy_in_my_head.wav")
+                        play_sound("theres_candy_in_my_head.wav")
                         pixels.fill(BLACK)
                     if packet.button == ButtonPacket.UP: # if button #2
                         pixels.fill(RED)
-                        playfile("open_my_head.wav")
+                        play_sound("open_my_head.wav")
                         pixels.fill(BLACK)
